ca9ffddb6e6a0df9c631788070b232c583b1bc4a
toleen98 <toleenballool98@gmail.com>
2020-10-16 19:09:30 +0300

    popupinfo / refactore style

--- src/App.js
+++ src/App.js
@@ -2,9 +2,42 @@ import React from 'react';
 import Navbar from './layouts/Navbar';
 import Slider from './components/Slider/Slider'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Movies from './components/Shows-components/Movies'
-import Tabs from './components/Tabs/Tabs'
+import Movies from './components/Shows-components/Movies';
+import Tabs from './components/Tabs/Tabs';
+import InfoPopUp from './components/popup/InfoPopUp'
 
+const movie = {
+  "Title": "Dora",
+  "Year": "2017",
+  "Rated": "N/A",
+  "Released": "31 Mar 2017",
+  "Runtime": "139 min",
+  "Genre": "Action, Horror, Thriller",
+  "Director": "Dass Ramasamy",
+  "Writer": "Dass Ramasamy",
+  "Actors": "Nayanthara, Harish Uthaman, Thambi Ramayya, Shan",
+  "Plot": "A father and a daughter purchase a used car which is possessed by an unknown dangerous spirit craving for revenge.",
+  "Language": "Tamil, Telugu, Hindi",
+  "Country": "India",
+  "Awards": "N/A",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BM2ViYTE1NjMtMGJjZC00NzJlLWE0MjMtNTRhZWQ0MWM3MGIwXkEyXkFqcGdeQXVyODk4ODEyMjk@._V1_SX300.jpg",
+  "Ratings": [
+      {
+          "Source": "Internet Movie Database",
+          "Value": "5.3/10"
+      }
+  ],
+  "Metascore": "N/A",
+  "imdbRating": "5.3",
+  "imdbVotes": "597",
+  "imdbID": "tt6080914",
+  "Type": "movie",
+  "DVD": "N/A",
+  "BoxOffice": "N/A",
+  "Production": "N/A",
+  "Website": "N/A",
+  "Response": "True"
+}
 function App() {
   return (
    
@@ -14,6 +47,7 @@ function App() {
         <Slider/>
         <Tabs/>
         <Movies/>
+        <InfoPopUp movie={movie}/>
        </Router>
       
     </div>
--- src/components/popup/InfoPopUp.js
+++ src/components/popup/InfoPopUp.js
@@ -1,38 +1,61 @@
 import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import './popup.css'
+
 
 
 const InfoPopUp = ({movie}) => {
     return (
-        <div>
+        <div className='popup' style={{backgroundImage: `url(${movie.Poster})`}}>
+            <div className='contant'>
             <div className='row1'>
-                <h2>{movie.Title}</h2>
-                <p>movie.Plot</p>
+                <h1>{movie.Title}</h1>
+                <p>{movie.Plot}</p>
             </div>
             <div className='row2'>
                 <div>
-                    <h3>Rating</h3>
-                        {movie.imdbRating}
+                    <h2>Rating</h2>
+                    <div>
+                        <Rating 
+                        name="half-rating-read" 
+                        defaultValue={movie.imdbRating / 2} 
+                        precision={0.5}
+                        getLabelText={()=> '#ffd600'} 
+                        color='#ffd600'
+                        readOnly 
+                        />
+                    </div>
+                        
+                </div>
+                <div className='genre'>
+                    <h2>Genre</h2>
+                    <p>{
+                        movie.Genre.split(',').map(gen => <span>{gen}</span>)
+                    }</p>
                 </div>
                 <div>
-                    <h3>Genre</h3>
-                    <p>{movie.Genre}</p>
-                </div>
-                <div>
-                    <h3>Released</h3>
+                    <h2>Released</h2>
                     <p>{movie.Released}</p>
                 </div>
                 <div>
-                    <h3>Directors</h3>
+                    <h2>Directors</h2>
                     <p>{movie.Director}</p>
                 </div>
-                <div>
-                    <h3>Language</h3>
-                    <p>{movie.Language}</p>
+                <div className='lang'>
+                    <h2>Language</h2>
+                    <p>
+                    {
+                        movie.Language.split(',').map(lang => <span>{lang}</span>)
+                    }</p>
                 </div>
             </div>
             <div>
-                <button>More Info</button>
+                <button className='btn'>More Info</button>
             </div>
+            </div>
+            
         </div>
     )
 }
+
+export default InfoPopUp;
